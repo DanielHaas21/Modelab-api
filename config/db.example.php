2a0d5dbb42be0665cab9c8abd431a0ecb7c4d657
Radek Titěra <rodak.titera@gmail.com>
2025-03-25 08:41:15 +0100

    Added config to .gitignore

--- config/db.example.php
+++ config/db.example.php
@@ -2,7 +2,7 @@
 
 const DB_CONFIG = [
     'servername' => '127.0.0.1',
-    'username'   => 'modelab_user',
-    'password'   => 'password',
+    'username'   => 'root',
+    'password'   => '',
     'database'   => 'modelab_api',
 ];
