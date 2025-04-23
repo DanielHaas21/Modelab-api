a19ace717bcc405f2101909f121840a52223b4b2
DanielHaas21 <daniel.haas@insio.cz>
2025-04-23 10:02:06 +0200

    fix: CORS preflight OPTIONS

--- index.php
+++ index.php
@@ -13,6 +13,16 @@ require_once __DIR__ . '/routes/tagRoutes.php';
 require_once __DIR__ . '/routes/assetRoutes.php';
 require_once __DIR__ . '/routes/fileRoutes.php';
 
+// things go bomboclat if this isnt here bcs preflight cors
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 $router->AddGET('/', function (Request $req, Response $res): void {
     $res->SetText('Modelab API');
 });
